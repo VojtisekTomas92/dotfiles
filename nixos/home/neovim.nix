cf371d8c212d00398bd060a3ae553fffaf18bc35
@@@ <@@@>
2024-12-13 11:07:48 +0100

    one dark colorscheme

--- nixos/home/neovim.nix
+++ nixos/home/neovim.nix
@@ -6,4 +6,11 @@
   programs.neovim = {
     enable = true;
   };
+
+  home.file.".config/nvim/init.lua".source = ../../neovim/init.lua;
+  home.file.".config/nvim/lua/config/lazy.lua".source = ../../neovim/lazy.lua;
+  home.file.".config/nvim/lua/plugins/" = {
+    source = ../../neovim/plugins;
+    recursive = true;
+  };
 }

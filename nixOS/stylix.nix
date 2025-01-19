12c904c4a9c4044c0a588e66fa7186d9778c50e9
Your Name <you@example.com>
2025-01-19 17:08:57 +0100

    Seems like i have to setupo stylinx outside of home manager

---
+++ nixOS/stylix.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  stylix = {
+    enable = true;
+    image = pkgs.fetchurl {
+      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    };
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
+  };
+}

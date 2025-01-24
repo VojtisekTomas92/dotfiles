d18214344c359e8a2f1094b170a9c91255a97212
Your Name <you@example.com>
2025-01-24 19:14:26 +0100

    Enable syncthing and nextcloud

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,12 +4,11 @@
   ...
 }: {
   imports = [
-    ./stylix.nix
+    ./plasma-manager.nix
   ];
   home.username = "tomas";
   home.homeDirectory = "/home/tomas";
   home.stateVersion = "24.11"; # Please read the comment before changing.
-
   home.packages = [
     pkgs.aw-server-rust
   ];
@@ -18,6 +17,7 @@
   };
 
   home.sessionVariables = {
+    FLAKE = "/home/tomas/dotfiles";
   };
 
   programs.bash = {
@@ -30,5 +30,12 @@
     package = pkgs.aw-server-rust;
   };
 
+  services.nextcloud-client = {
+    enable = true;
+    startInBackground = true;
+  };
+  services.syncthing = {
+    enable = true;
+  };
   programs.home-manager.enable = true;
 }

7b173051ed3d1f61d5304ebc223a2dd47eb9111e
Tomas <vojtisektomas92@gmail.com>
2025-05-03 19:29:36 +0200

    Add nix stuff

---
+++ nix/home.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}:
+
+{
+
+  home.homeDirectory = "/home/tomas";
+  home.stateVersion = "24.05";
+  home.username = "tomas";
+
+  home.packages = [pkgs.alejandra pkgs.nixd];
+
+  programs = {
+    home-manager = {
+      enable = true;
+    };
+
+    git = {
+      enable = true;
+      userName = "Tomas";
+      userEmail = "vojtisektomas92@gmail.com";
+    };
+
+    zsh = {
+      enable = true;
+    };
+  };
+
+}

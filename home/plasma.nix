4599916a82f5b949e45a6111ef68dff3d8082543
Tomas Vojtisek <vojtisektomas92@gmail.com>
2026-05-03 08:28:56 +0200

    add plasma settings

--- home/plasma.nix
+++ home/plasma.nix
@@ -3,6 +3,31 @@
   programs.plasma = {
     enable = true;
 
+    fonts = {
+      fixedWidth = {
+        family = "FiraCode Nerd Font Mono";
+        pointSize = 10;
+      };
+      general = {
+        family = "Fira Sans";
+        pointSize = 11;
+      };
+      windowTitle = {
+        family = "Fira Sans";
+        pointSize = 11;
+      };
+      menu = {
+        family = "Fira Sans";
+        pointSize = 11;
+      };
+    };
+
+    input = {
+      keyboard = {
+        numlockOnStartup = "on";
+      };
+    };
+
     hotkeys.commands."launch-konsole" = {
       name = "Launch Konsole";
       key = "Meta+Space";
@@ -13,6 +38,12 @@
       lookAndFeel = "org.kde.breezedark.desktop";
       theme = "breeze-dark";
       colorScheme = "BreezeDark";
+    };
+
+    session = {
+      sessionRestore = {
+        restoreOpenApplicationsOnLogin = "startWithEmptySession";
+      };
     };
   };
 }
